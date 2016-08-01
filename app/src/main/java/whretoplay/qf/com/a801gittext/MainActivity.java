b17046811fec1f5c938d2e16abddc8b8caba307d
bostonhulmes <1524673422@qq.com>
2016-08-01 20:36:01 +0800

    第二次提交整个项目

--- app/src/main/java/whretoplay/qf/com/a801gittext/MainActivity.java
+++ app/src/main/java/whretoplay/qf/com/a801gittext/MainActivity.java
@@ -2,9 +2,12 @@ package whretoplay.qf.com.a801gittext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
